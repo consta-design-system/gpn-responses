990fae0db21299ed26a700d7bd363eacff982dc5
N1MBER <socialfox33@gmail.com>
2022-09-14 17:25:03 +0300

    fix(Sandbox): rewrite links to ids

--- src/components/Responses/__stand__/Responses.stand.tsx
+++ src/components/Responses/__stand__/Responses.stand.tsx
@@ -7,6 +7,6 @@ export default createStand({
   description: 'Cообщения о важных статусах',
   figma: 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fv9Jkm2GrymD277dIGpRBSH%2FConsta-UI-Kit%3Fnode-id%3D6263%253A116157',
   version: '1.0.2',
-  sandbox: 'https://codesandbox.io/embed/responses-51p7y7?fontsize=14&hidenavigation=1&theme=dark',
+  sandbox: 'responses-51p7y7',
   status: 'stable',
 });
