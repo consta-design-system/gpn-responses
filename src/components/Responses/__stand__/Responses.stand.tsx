e379138d927a70142e4f54f8178bb97cf6d79759
arhayka <arhayka@gmail.com>
2022-09-15 13:29:06 +0300

    docs: change imports, add image (#12)
    * docs: change imports, add image
    
    * ee
    
    Co-authored-by: gizeasy <gizeasy@gmail.com>

--- src/components/Responses/__stand__/Responses.stand.tsx
+++ src/components/Responses/__stand__/Responses.stand.tsx
@@ -5,7 +5,8 @@ export default createStand({
   id: 'Responses',
   group: 'components',
   description: 'Cообщения о важных статусах',
-  figma: 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fv9Jkm2GrymD277dIGpRBSH%2FConsta-UI-Kit%3Fnode-id%3D6263%253A116157',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fv9Jkm2GrymD277dIGpRBSH%2FConsta-UI-Kit%3Fnode-id%3D6263%253A116157',
   version: '1.0.2',
   status: 'stable',
 });
