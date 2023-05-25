fe8af3dc4fa31a0028253eef9dff67b6576adcc5
gizeasy <gizeasy@gmail.com>
2023-05-25 15:20:49 +0300

    docs(standConfig): add figmaUrl

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -23,4 +23,6 @@ export const { createStand } = createConfig({
   description:
     'Сообщения об ошибках с иллюстрациями по теме добычи и переработки нефти.',
   repositoryUrl: 'https://github.com/consta-design-system/gpn-responses',
+  figmaUrl:
+    'https://www.figma.com/file/v9Jkm2GrymD277dIGpRBSH/Consta-UI-Kit?type=design&node-id=6263-116157&t=kKryFkzj99IXSSgO-0',
 });
